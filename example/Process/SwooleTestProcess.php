e21154c389e025253382d972b98d5cd7324a2eb0
Yurun <admin@yurunsoft.com>
2021-09-15 15:22:17 +0800

    imi-amqp 兼容非 Swoole

--- example/Process/SwooleTestProcess.php
+++ example/Process/SwooleTestProcess.php
@@ -6,10 +6,12 @@ namespace AMQPApp\Process;
 
 use Imi\AMQP\Contract\IConsumer;
 use Imi\Aop\Annotation\Inject;
-use Imi\App;
+use Imi\Event\Event;
+use Imi\Log\Log;
 use Imi\Swoole\Process\Annotation\Process;
 use Imi\Swoole\Process\BaseProcess;
-use Swoole\Coroutine;
+use Imi\Swoole\Util\Coroutine;
+use Imi\Util\ImiPriority;
 
 /**
  * @Process(name="TestProcess")
@@ -30,11 +32,21 @@ class SwooleTestProcess extends BaseProcess
      */
     protected $testConsumer2;
 
+    private bool $running = false;
+
     public function run(\Swoole\Process $process): void
     {
+        $this->running = true;
         $this->runConsumer($this->testConsumer);
         $this->runConsumer($this->testConsumer2);
-        \Swoole\Coroutine::yield();
+        $channel = new \Swoole\Coroutine\Channel();
+        Event::on('IMI.PROCESS.END', function () use ($channel) {
+            $this->running = false;
+            $this->testConsumer->close();
+            $this->testConsumer2->close();
+            $channel->push(1);
+        }, ImiPriority::IMI_MAX);
+        $channel->pop();
     }
 
     private function runConsumer(IConsumer $consumer): void
@@ -46,11 +58,12 @@ class SwooleTestProcess extends BaseProcess
             }
             catch (\Throwable $th)
             {
-                /** @var \Imi\Log\ErrorLog $errorLog */
-                $errorLog = App::getBean('ErrorLog');
-                $errorLog->onException($th);
-                sleep(3);
-                $this->runConsumer($consumer);
+                Log::error($th);
+                if ($this->running)
+                {
+                    sleep(3);
+                    $this->runConsumer($consumer);
+                }
             }
         });
     }
