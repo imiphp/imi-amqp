a28dfedffffb02f99d9feb62e6cd884a7882bcfe
Yurun <admin@yurunsoft.com>
2022-06-24 08:58:39 +0800

    支持配置启动服务时检测数据库、Redis 等是否可用（Swoole、Workerman） (#358)

---
+++ src/Listener/CheckPoolResource.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imi\AMQP\Listener;
+
+use Imi\AMQP\Pool\AMQPPool;
+use Imi\Bean\Annotation\Listener;
+use Imi\Config;
+use Imi\Event\EventParam;
+use Imi\Event\IEventListener;
+use Imi\Log\Log;
+use Imi\Pool\PoolManager;
+
+/**
+ * @Listener(eventName="IMI.CHECK_POOL_RESOURCE")
+ */
+class CheckPoolResource implements IEventListener
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function handle(EventParam $e): void
+    {
+        if ($connections = Config::get('@app.amqp.connections'))
+        {
+            $result = &$e->getData()['result'];
+            foreach ($connections as $name => $_)
+            {
+                if (!PoolManager::exists($name))
+                {
+                    try
+                    {
+                        $instance = AMQPPool::getInstance($name);
+                        if ($instance->isConnected())
+                        {
+                            $instance->close();
+                        }
+                        else
+                        {
+                            $result = false;
+                        }
+                    }
+                    catch (\Throwable $th)
+                    {
+                        Log::error($th);
+                        Log::error(sprintf('The AMQP [%s] are not available', $name));
+                        $result = false;
+                    }
+                }
+            }
+        }
+    }
+}
